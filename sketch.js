1e8f7490f6cc5bf072e4fe7b682aa854eba7a094
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-26 19:51:28 -0500

    ballStick
    balls and lines changing colors in a grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-
+  createCanvas(700,700)
 }
 
 function draw() {
+  background(255)
+  for (var i=0; i<width; i+=20){
+    for (var j=0; j<height; j+=20){
+      new ballStick(i,j,10)
+    }
+  }
+}
 
+var ballStick = function(x,y,ellipseSize){
+  var colors = color(random(0,255),random(0,255),random(0,255))
+  fill(colors)
+  stroke(colors)
+  strokeWeight(8)
+  line(x,y,mouseX,mouseY)
+  ellipse(x,y,ellipseSize, ellipseSize)
 }
