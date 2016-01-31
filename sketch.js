29dfac001b41b148692cc04c94289c80d740ff36
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-31 15:06:51 -0500

    background
    change background on mousePressed

--- sketch.js
+++ sketch.js
@@ -1,32 +1,30 @@
 function tri (w,h){
-  var x=30
-  var y=30
+  var x=10
+  var y=10
   
   for(var i=0; i<w; i=i+6){
     for(var j=0;j<h;j=j+6){
-      var colors = color(random(55,255),random(0,100),random(55,255))
+      strokeWeight(6)
+      var colors = color(random(0,255),random(0,55),random(0,255))
       fill(colors)
       stroke(colors)
-      strokeWeight(2)
       line(x, y,mouseX, mouseY);
-      x=x+60
+      x=x+45
     }
-    y=y+60
-    x=30
+    y=y+45
+    x=10
   }
 }
 
 function setup () {
-  createCanvas(600,600);
+  createCanvas(700,700);
 }
 
-var t=0
-
 function draw() {
-  background(255,255,255,10)
-  tri (50,50)
-  t=t+.3
-  if (t>1){
-    t=0
-  }
+  tri (100,100)
+}
+
+function mousePressed() {
+  background(200,200,255,40)
+  
 }
