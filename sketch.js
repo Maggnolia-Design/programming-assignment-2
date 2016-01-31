9d76cdfb36e42958c74b2d9bbde33f322ceb7d77
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-31 16:10:27 -0500

    distance
    print the distance

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+var circum
+
+function circle (x,y){
+  fill(x,200,x)
+  noStroke()
+  ellipse(x,y,circum,circum)
+}
+
 function setup () {
   createCanvas(600,600);
 }
 
 function draw() {
+  background(0,0,0)
+  circum =mouseX/2
+  circle(mouseX,300)
+  circum=600-mouseX
+  circle(600-mouseX,300)
 
+  print(mouseX-(600-mouseX))
+  fill(0,0,0)
+  rect(200,292,100,35)
+  fill(255,255,255)
+  textSize(18)
+  text(print,220,300,50,20)
 }
