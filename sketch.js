2e01f6b0f6f1615a3e4039cdd803ff45de8ae024
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-31 22:17:25 -0500

    follow mouse
    text follows mouseX

--- sketch.js
+++ sketch.js
@@ -12,14 +12,14 @@ function setup () {
 
 function draw() {
   background(0,0,0)
-  circum =mouseX/2
-  circle(mouseX,300)
   circum=600-mouseX
   circle(600-mouseX,300)
+  circum =mouseX/2
+  circle(mouseX,300)
 
   fill(0,0,0)
-  rect(200,292,100,35)
+  rect(mouseX-10,343,56, 35)
   fill(255,255,255)
   textSize(18)
-  text((mouseX-(600-mouseX)).toString(),220,300,50,20)
+  text((mouseX-(600-mouseX)).toString(),mouseX,350,50,20)
 }
